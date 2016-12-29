ed173890fcc124a03c5f09ef5b99365e17431795
aubed <aubed@ES05567.fae.net>
2016-12-29 11:23:14 +0100

    Added more actions and change game logical

--- HMS+/src/classes/actions.java
+++ HMS+/src/classes/actions.java
@@ -1,6 +1,7 @@
 package classes;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class actions {
 	public actions(){}
@@ -9,12 +10,65 @@ public class actions {
 		ArrayList<String> Action = new ArrayList<String>();
 		//int 
 		Action.add(alias + " se eleva desde el extremo y bate al portero.");
-		return Action.get(0);
+		Action.add(alias + " apura y suelta una rosca baja para batir al portero.");
+		Action.add(alias + " bate a su defensor y se cuela entre dos.");
+		Action.add(alias + " ve la salida del portero y anota de vaselina.");
+		Action.add("Increible fly de " + alias + " que recibio el balon en el aire desde el central.");
+		Random rn = new Random();
+		return Action.get(rn.nextInt(Action.size()));
 	}
 	public String BrGoal(String alias){
 		ArrayList<String> Action = new ArrayList<String>();
 		//int 
+		Action.add(alias + " arma y lanza un potente disparo desde 8 metros para batir al portero.");
+		Random rn = new Random();
+		return Action.get(rn.nextInt(Action.size()));
+	}
+	public String CeGoal(String alias){
+		ArrayList<String> Action = new ArrayList<String>();
+		//int 
+		Action.add(alias + " recibe un pase filtrado del central y bate al portero.");
+		Random rn = new Random();
+		return Action.get(rn.nextInt(Action.size()));
+	}
+	public String CrGoal(String alias){
+		ArrayList<String> Action = new ArrayList<String>();
+		//int 
+		Action.add(alias + " anota un impresionante gol con un disparo de cadera");
+		Random rn = new Random();
+		return Action.get(rn.nextInt(Action.size()));
+	}
+	//Fail Actions
+	public String ExFail(String alias){
+		ArrayList<String> Action = new ArrayList<String>();
+		//int 
 		Action.add(alias + " se eleva desde el extremo y bate al portero.");
-		return Action.get(0);
+		Action.add(alias + " apura y suelta una rosca baja para batir al portero.");
+		Action.add(alias + " bate a su defensor y se cuela entre dos.");
+		Action.add(alias + " ve la salida del portero y anota de vaselina.");
+		Action.add("Increible fly de " + alias + " que recibio el balon en el aire desde el central.");
+		Random rn = new Random();
+		return Action.get(rn.nextInt(Action.size()));
+	}
+	public String BrFail(String alias){
+		ArrayList<String> Action = new ArrayList<String>();
+		//int 
+		Action.add(alias + " pierde el balon");
+		Random rn = new Random();
+		return Action.get(rn.nextInt(Action.size()));
+	}
+	public String CeFail(String alias){
+		ArrayList<String> Action = new ArrayList<String>();
+		//int 
+		Action.add(alias + " pierde el balon");
+		Random rn = new Random();
+		return Action.get(rn.nextInt(Action.size()));
+	}
+	public String CrFail(String alias){
+		ArrayList<String> Action = new ArrayList<String>();
+		//int 
+		Action.add(alias + " pierde el balon");
+		Random rn = new Random();
+		return Action.get(rn.nextInt(Action.size()));
 	}
 }
--- HMS+/src/game.java
+++ HMS+/src/game.java
@@ -1,37 +1,52 @@
 import classes.player;
 import classes.team;
 
-public class game {
+import java.util.Random;
 
+import classes.actions;
+
+public class game {
+	private static team Home = new team();
+	private static team Away = new team();
+	private static int possesion;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		team Home=new team();
-		team Away=new team();
-		int HomeAVG=80;
-		int AwayAVG=55;
 		int nactions=0;
+		Random rn = new Random();
+		possesion = rn.nextInt(2);
+		System.out.println ("El arbitro procede al sorteo del saque.");
+		String SAction="";
+		Home =new team("Barcelona");
+		Away =new team("Huesca");
 		for (int p=1; p<=2;p++){
 			if (p==1){ // Printing start first time
-				System.out.println ("Primera parte <=> Marcador: " + Home.getGoals() +" - " + Away.getGoals());
+				System.out.println ("Primera parte <=> Marcador: "+ Home.getName() + " " + Home.getGoals() +" - " + Away.getGoals() +" " +Away.getName());
 				System.out.println ("Comienza el Partido");
 				System.out.println ("");
 			}
 			else { // Printing start second time
 				System.out.println ("");
-				System.out.println ("Segunda parte <=> Marcador: " + Home.getGoals() +" - " + Away.getGoals());
+				System.out.println ("Segunda parte <=> Marcador: "+ Home.getName() + " " + Home.getGoals() +" - " + Away.getGoals() +" " +Away.getName());
 				System.out.println ("Comienza el Partido");
 				System.out.println ("");
 			}
 			for (int m=0; m<30;m++){
-				//System.out.println ("Min " + m + ": ");
 				nactions=getActions();
 				for (int i=1; i<=nactions; i++){
-					System.out.println ("Actions " + i + ": " );
+					SAction="Min " + m + ": " + NewAction();
+					String attackTeam="";
+					if (possesion==1){
+						attackTeam="Away";
+					} else {
+						attackTeam="Home";	
+					}
+					System.out.println(SAction+" => "+attackTeam);
+					ChangePosession();
 				}
 			}
 		}
 		System.out.println ("");
-		System.out.println ("Final del Partido <=> Marcador: " + Home.getGoals() +" - " + Away.getGoals());
+		System.out.println ("Final del Partido <=> Marcador: "+ Home.getName() + " " + Home.getGoals() +" - " + Away.getGoals() +" " +Away.getName());
 		System.out.println ("");
 	}
 	private static int getActions(){ //Ratio for actions (55 => One Action per minute; 35 => One Action per minute;10 => One Action per minute;)
@@ -47,4 +62,98 @@ public class game {
 		}
 		return nactions;
 	}
+	private static void ChangePosession(){
+		if (possesion==0){
+			possesion=1;
+		} else {
+			possesion=0;
+		}
+	}
+	private static String NewAction(){ 
+		Random rn = new Random();
+		Random rnAction = new Random();
+		String SAction;
+		int DiffValue;
+		int HomeAVG=55;
+		int AwayAVG=80;
+		if (possesion > 1) { DiffValue=HomeAVG-AwayAVG;}
+		else {DiffValue=HomeAVG-(AwayAVG-((int) Math.round((HomeAVG-AwayAVG)/2)));}
+		if (DiffValue<0) {DiffValue=DiffValue*(-1);}
+		if (rnAction.nextInt(100)+1>DiffValue){
+			SAction=Goal(rn.nextInt(6)+1) + " " + DiffValue;
+		} else {
+			SAction="Fallo"  + " " + DiffValue;
+		}
+		return SAction; 
+	}
+	private static String Goal(int p){ 
+		actions Action=new actions();
+		String SGoal;
+		if (possesion==1){
+			Away.Goal();
+		} else {
+			Home.Goal();			
+		}
+		switch (p){
+		case 1:
+		case 5:
+			SGoal=Action.ExGoal("Jugador");
+			break;
+		case 2:
+		case 4:
+			SGoal=Action.BrGoal("Jugador");
+			break;
+		case 3:
+			SGoal=Action.CrGoal("Jugador");
+			break;
+		default:
+			SGoal=Action.CeGoal("Jugador");
+			break;
+		}
+		return SGoal;
+	}
+	private static String Fail(int p){ 		
+		actions Action=new actions();
+		String SFail;
+		switch (p){
+		case 1:
+		case 5:
+			SFail=Action.ExFail("Jugador");
+			break;
+		case 2:
+		case 4:
+			SFail=Action.BrFail("Jugador");
+			break;
+		case 3:
+			SFail=Action.CrFail("Jugador");
+			break;
+		default:
+			SFail=Action.CeFail("Jugador");
+			break;
+		}
+		return SFail;
+	}
+	private static String Exclution(int p){ 
+		actions Action=new actions();
+		String SExclution;
+		switch (p){
+		case 1:
+		case 5:
+			SExclution=Action.ExFail("Jugador");
+			break;
+		case 2:
+		case 4:
+			SExclution=Action.BrFail("Jugador");
+			break;
+		case 3:
+			SExclution=Action.CrFail("Jugador");
+			break;
+		default:
+			SExclution=Action.CeFail("Jugador");
+			break;
+		}
+		return SExclution;
+	}
+	
+	
 }
